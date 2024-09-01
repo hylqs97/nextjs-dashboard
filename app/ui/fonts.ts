a1bc2dfb33c4c4b5b97fc701e6540d0b3a357c57
侯雨松 <403683723@qq.com>
2024-09-01 16:48:40 +0800

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana} from "next/font/google";
+import {bold} from "next/dist/lib/picocolors";
+
+export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
